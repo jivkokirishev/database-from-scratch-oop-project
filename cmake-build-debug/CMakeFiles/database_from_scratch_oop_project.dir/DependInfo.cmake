26f5509a27c79ffc99ec6523f49d2a287e640637
jivkokirishev <jivkokirishev@gmail.com>
2019-04-26 12:50:57 +0300

    Develop branch initial commit. I should have commited it before the feature-collections branch commits.

---
+++ cmake-build-debug/CMakeFiles/database_from_scratch_oop_project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/main.cpp" "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/cmake-build-debug/CMakeFiles/database_from_scratch_oop_project.dir/main.cpp.o"
+  "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/src/collections/Collection.cpp" "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/cmake-build-debug/CMakeFiles/database_from_scratch_oop_project.dir/src/collections/Collection.cpp.o"
+  "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/src/collections/doubly_linked_list/DoublyLinkedList.cpp" "/home/jivko/storage/dev projects/c++/database-from-scratch-oop-project/cmake-build-debug/CMakeFiles/database_from_scratch_oop_project.dir/src/collections/doubly_linked_list/DoublyLinkedList.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/database_from_scratch_oop_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/database_from_scratch_oop_project.dir/main.cpp.o"
+  "CMakeFiles/database_from_scratch_oop_project.dir/src/collections/Collection.cpp.o"
+  "CMakeFiles/database_from_scratch_oop_project.dir/src/collections/doubly_linked_list/DoublyLinkedList.cpp.o"
+  "database_from_scratch_oop_project.pdb"
+  "database_from_scratch_oop_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/database_from_scratch_oop_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
